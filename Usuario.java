e7deea70c984b343e5c3c2202cc43aaff2c52a82
Ari Hsieh <arihsiehlu@gmail.com>
2020-04-05 19:16:19 -0300

    Se adicionan los getters, setters y constructores

--- Usuario.java
+++ Usuario.java
@@ -10,4 +10,54 @@ public class Usuario {
     private int cantHijos;
     private int saldo;
     
+    //Get´s y Set´s
+    public void setNombre(String unNombre){
+        nombre = unNombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setCedula(int unaCedula){
+        cedula = unaCedula;
+    }
+    public int getCedula(){
+        return cedula;
+    }
+    public void setContrasenia(String unaContrasenia){
+        contrasenia = unaContrasenia;
+    }
+    public String getContrasenia(){
+        return contrasenia;
+    }
+    public void setEdad(int unaEdad){
+        edad = unaEdad;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setCantHijos(int unaCantHijos){
+        cantHijos = unaCantHijos;
+    }
+    public int getCantHijos(){
+        return cantHijos;
+    }
+    public void setSaldo(int unSaldo){
+        saldo = unSaldo;
+    }
+    public int getSaldo(){
+        return saldo;
+    }
+    
+    //Constructores
+    public Usuario(){
+        this.setSaldo(0);
+    }
+    public Usuario (String unNombre, int unaCedula, String unaContrasenia, int unaEdad, int unaCantHijos, int unSaldo){
+       this.setNombre(unNombre);
+       this.setCedula(unaCedula);
+       this.setContrasenia(unaContrasenia);
+       this.setEdad(unaEdad);
+       this.setCantHijos(unaCantHijos);
+       this.setSaldo(unSaldo);
+    }
 }
