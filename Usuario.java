162041df2628477a560ab34dce2bcdb719ba8145
Ari Hsieh <arihsiehlu@gmail.com>
2020-04-05 18:47:25 -0300

    Se crea la clase usuario con los atributos nombre, cedula, contrasenia, edad, cantidad de hijos y saldo

---
+++ Usuario.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Usuario {
+    
+    //Atributos
+    private String nombre; 
+    private int cedula;
+    private String contrasenia;
+    private int edad;
+    private int cantHijos;
+    private int saldo;
+    
+}
